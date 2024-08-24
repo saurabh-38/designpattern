2113566985d28784247da7cd7f93ed4f20ec2a91
saurabh <guptasaurabh5858@gmail.com>
2024-08-24 17:50:37 +0530

    singleton

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,19 @@ public class Main {
     public static void main(String[] args) {
 
         //System.out.println("Hello world!");
-
-        House h = new House.Builder()
-                .rent(5)
-                .address("delhi")
-                .build();
-      System.out.println(h.toString());
+//
+//        House h = new House.Builder()
+//                .rent(5)
+//                .address("delhi")
+//                .build();
+//      System.out.println(h.toString());
       //adassert 1==2;
+
+     Singleton s1 = Singleton.getInstance(2,5);
+
+        Singleton s2 = Singleton.getInstance(7,5);
+        System.out.println(s1.toString());
+        System.out.println(s2.toString());
 
 
     }
--- src/Singleton.java
+++ src/Singleton.java
@@ -1,7 +1,30 @@
 public class Singleton {
-    int a,b;
-    Singleton(int a,int b){
+    //
+
+    private int a;
+    private int b;
+    private static Singleton instance;
+
+    private  Singleton(int a,int b){
         this.a=a;
         this.b=b;
+    }
+
+    @Override
+    public String toString() {
+        return "Singleton{" +
+                "a=" + a +
+                ", b=" + b +
+                '}';
+    }
+
+    public static Singleton getInstance(int a, int b) {
+        if(instance==null){
+            instance=new Singleton(a,b);
+        }else{
+            instance.a=a;
+            instance.b=b;
+        }
+        return instance;
     }
 }
